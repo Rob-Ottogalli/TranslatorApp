06c20abb93c31e32b06ef22ae8da9afc555c00d7
Robert Ottogalli <59754310+Rob-Ottogalli@users.noreply.github.com>
2022-03-23 22:55:47 -0500

    Updated PostProcessor export. Replaces text, exports target file.  Does not replace source text for strings ending in "

--- src/main/java/com/translatorapp/PostProcessor.java
+++ src/main/java/com/translatorapp/PostProcessor.java
@@ -1,10 +1,10 @@
 package com.translatorapp;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class PostProcessor {
@@ -46,31 +46,62 @@ public class PostProcessor {
         if (this.outputFilename == null || this.outputFilename == "" ||
                 !this.outputFilename.equals(outputFilename)) {
             this.outputFilename = "target_" + this.sourceFilename;
-            System.out.println("Stu gots");
+//            System.out.println("Stu gots");
         }
-        this.outputFilename = "target_" + this.sourceFilename;
+//        this.outputFilename = "target_" + this.sourceFilename;
 
+//        try {
+//            // Create a file based on the current filename.
+//            File targetFile = new File(this.outputFilename);
+//
+//            // If the output log file does not exist, create a new one,
+//            // otherwise, append to the existing output log file.
+//            if (!targetFile.exists()) {
+//                targetFile.createNewFile();
+//            }
+//            FileWriter fWriter = new FileWriter(targetFile, false);
+//            BufferedWriter bWriter = new BufferedWriter(fWriter);
+//            out = new PrintWriter(bWriter);
+//
+//            // Write to the appropriate output file.
+//            for (int i=0; i < this.parsedLines.size(); i++) {
+//                String targetText = this.parsedLines.get(i).getTargetText();
+//                out.println(targetText);
+//            }
+//
+//            out.flush();
+//            System.out.println(this.outputFilename);
+//        }
         try {
-            // Create a file based on the current filename.
-            File targetFile = new File(this.outputFilename);
-
-            // If the output log file does not exist, create a new one,
-            // otherwise, append to the existing output log file.
-            if (!targetFile.exists()) {
-                targetFile.createNewFile();
+            //Instantiating the Scanner class to read the file
+            Scanner sc = new Scanner(new File(this.sourceFilename));
+            //instantiating the StringBuffer class
+            StringBuffer buffer = new StringBuffer();
+            //Reading lines of the file and appending them to StringBuffer
+            while (sc.hasNextLine()) {
+                buffer.append(sc.nextLine()+System.lineSeparator());
             }
-            FileWriter fWriter = new FileWriter(targetFile, false);
-            BufferedWriter bWriter = new BufferedWriter(fWriter);
-            out = new PrintWriter(bWriter);
+            String fileContents = buffer.toString();
+            System.out.println("Contents of the file: "+fileContents);
 
-            // Write to the appropriate output file.
+            //closing the Scanner object
+            sc.close();
+
+            //Replacing the old line with new line
+                        // Write to the appropriate output file.
             for (int i=0; i < this.parsedLines.size(); i++) {
+                String sourceText = this.parsedLines.get(i).getSourceText();
                 String targetText = this.parsedLines.get(i).getTargetText();
-                out.println(targetText);
+                fileContents = fileContents.replaceAll(sourceText, targetText);
             }
 
-            out.flush();
-            System.out.println(this.outputFilename);
+
+            //instantiating the FileWriter class
+            FileWriter writer = new FileWriter(this.outputFilename);
+            System.out.println("");
+            System.out.println("new data: "+fileContents);
+            writer.append(fileContents);
+            writer.flush();
         }
         catch (Exception e) {
             throw new IllegalStateException(e);
